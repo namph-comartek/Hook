4db484d5444eaf968fce5f23ba53ace20aa32116
nam-comartek <hayvuilen082@gmail.com>
2022-03-15 17:34:39 +0700

    15-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, memo, useMemo, useRef } from "react";
+import Content from "./Content";
+import "./App.css";
 function App() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [product, setProduct] = useState([]);
+  const nameRef = useRef();
+  const handleSubmit = () => {
+    setProduct([...product, { name, price: +price }]);
+    setName("");
+    setPrice("");
+    nameRef.current.focus();
+  };
+  const total = useMemo(() => {
+    return product.reduce((total, item) => {
+      return total + item.price;
+    }, 0);
+  }, [product]);
+  const [user, setUser] = useState([]);
+  const getData = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input ref={nameRef} value={name} placeholder="Enter name product" onChange={(e) => setName(e.target.value)} />
+      <br />
+      <input value={price} placeholder="Enter price product" onChange={(e) => setPrice(e.target.value)} />
+      <br />
+      <button onClick={handleSubmit}> Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {product.map((product, index) => (
+          <li key={index}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button onClick={getData}>Get Data</button>
+        <ul>
+          {user.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>{item.email}</p>
+              <p>{item.phone}</p>
+              <p>{item.website}</p>
+              <p>{item.company.name}</p>
+              <p>{item.company.catchPhrase}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,5 @@
+function Content() {
+  console.log("Content");
+  return <h2> Hello Hoài Nam</h2>;
+}
+export default Content;
